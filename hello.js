ad0998ef33e18e8b955422e0d5856723a5b5978a
bedrierol <bedrierol@gmail.com>
2016-11-22 23:57:58 +0200

    qwe

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World bedri\n");
+  response.end("Hello World erol\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
