7cfd2bf2ff9100bd0734e3efcdbe83f1704a4906
bedrierol <bedrierol@gmail.com>
2016-11-23 22:00:02 +0300

    some fixes

--- hello.js
+++ hello.js
@@ -1,13 +1,10 @@
-/*eslint-env node*/
+var express = require('express')
+var app = express()
 
-// Load the http module to create an http server.
-var http = require('http');
+app.get('/', function (req, res) {
+  res.send('asd')
+})
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World qwers\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
